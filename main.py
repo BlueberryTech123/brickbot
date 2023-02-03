cd468aff561a269c570eae8b6064399ffa683071
JesseIguess <blueberry21007@gmail.com>
2023-02-03 08:21:10 +0000

    funni ping command

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ import discord
 
 TOKEN = os.environ['TOKEN']
 
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print("Bot Online")
+
+@bot.slash_command(name = "ping", description = "Check latency")
+async def hello(ctx):
+    await ctx.respond(f"Ping! :ping_pong: **{bot.latency} ms**")
+
 bot.run(TOKEN)
