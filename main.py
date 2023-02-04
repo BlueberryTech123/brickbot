4f59ab185bb95a6224d0d073377fbbd900e62c29
JesseIguess <blueberry21007@gmail.com>
2023-02-04 01:40:06 +0000

    no

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 import discord
 import math
+from dotenv import load_dotenv
 
 print("Loading bot...")
 
-TOKEN = os.environ["TOKEN"]
+load_dotenv()
+print(os.environ)
+TOKEN = os.environ.get("TOKEN")
 
 bot = discord.Bot()
 
@@ -13,7 +16,7 @@ async def on_ready():
     print("Bot Online")
 
 @bot.slash_command(name = "ping", description = "Check latency")
-async def hello(ctx):
-    await ctx.respond(f"Ping! :ping_pong:\n **{round(bot.latency, 3)} miliseconds**")
+async def ping(ctx):
+    await ctx.respond(f"Ping! :ping_pong:\n **{round(bot.latency * 1000, 3)} miliseconds**")
 
 bot.run(TOKEN)
